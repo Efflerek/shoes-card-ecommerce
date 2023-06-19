5cd36dde746d225219dcf220ebdf64347a0a0327
Efflerek <piotr.effler@gmail.com>
2023-06-19 18:29:43 +0100

    Updated css after adding js

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-const container = document.querySelector(".container");
-const color1 = document.querySelector(".color-1");
-const color2 = document.querySelector(".color-2");
+const container - document.querySelector('.container');
+const color1 = document.querySelector('.color-1');
+const color2 = document.querySelector('.color-2');
 
-color2.addEventListener("click", () => {
-  container.classList.add("change");
-});
+color2.addEventListener('click', () => {
+  container.classList.add('change')
+})
 
-color1.addEventListener("click", () => {
-  container.classList.remove("change");
-});
